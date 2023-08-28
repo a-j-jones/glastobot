c891441fda9ed935e99e6d666cda0b89836449ff
adamj <a.j.jones@sky.com>
2023-08-29 00:37:26 +0100

    Updated GUI code formatting

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 
 # Global constants
-BASE_COUNT = "4"
-BASE_URL = "https://www.glastonburyfestivals.co.uk/information/tickets/"
+base_count = "4"
+base_url = "https://www.glastonburyfestivals.co.uk/information/tickets/"
 PROXY = ""  # No proxy was provided in original code. Should be filled if used.
 
 
@@ -49,44 +49,60 @@ def threaded_execution(elements, function):
 
 
 class GlastoGUI(tk.Tk):
+    base_count = "4"
+    base_url = "https://www.glastonburyfestivals.co.uk/information/tickets/"
+    x_pad = 10
+    y_pad = 2
+
     def __init__(self):
         super(GlastoGUI, self).__init__()
+        self._setup_attributes()
+        self._setup_tkinter_window()
+        self._create_controls()
+        self.mainloop()
 
-        # GlastoManager object:
+    def _setup_attributes(self):
+        """Initializes class attributes."""
         self.manager = None
         self.execution_thread = None
         self.url_thread = None
-
-        # Driver info:
         self.driver_info = []
+        self.checkboxes = []
+        self.checkvars = []
 
-        # TKinter window setup:
+    def _setup_tkinter_window(self):
+        """Configures the main tkinter window."""
         self.title("GlastoBot")
+        self.wm_attributes("-topmost", 1)
 
-        self.x_pad = 10
-        self.y_pad = 2
+    def _create_controls(self):
+        """Creates and sets up GUI controls."""
+        # URL entry and controls
+        self._create_url_controls()
 
-        # URL entry:
+        # Checkbox area for URLs
+        self._create_checkbox_area()
+
+    def _create_url_controls(self):
+        """Creates URL entry and related controls."""
         self.url_label = tk.Label(self, text="URL:")
         self.url_label.grid(row=0, column=0, padx=self.x_pad, pady=self.y_pad, sticky="W")
         self.url_entry = tk.Entry(self, width=55)
         self.url_entry.grid(row=0, column=1, columnspan=3, padx=self.x_pad, pady=self.y_pad, sticky="W")
-        self.url_entry.insert(0, BASE_URL)
+        self.url_entry.insert(0, self.base_url)
 
-        # Driver count selection (integer only) default 9:
+        # Driver count selection (integer only) default 9
         self.driver_count_label = tk.Label(self, text="Driver Count:")
         self.driver_count_label.grid(row=1, column=0, padx=self.x_pad, pady=self.y_pad, sticky="W")
         self.driver_count_entry = tk.Entry(self)
         self.driver_count_entry.grid(row=1, column=1, padx=self.x_pad, pady=self.y_pad, sticky="W")
-        self.driver_count_entry.insert(0, BASE_COUNT)
+        self.driver_count_entry.insert(0, self.base_count)
 
         self.start_button = tk.Button(self, text="Start", command=self.start)
         self.start_button.grid(row=0, column=5, rowspan=2, padx=self.x_pad, pady=self.y_pad, sticky="W")
 
-        # Window always on top:
-        self.wm_attributes("-topmost", 1)
-
-        # URL Checkboxes:
+    def _create_checkbox_area(self):
+        """Creates area with checkboxes to pause/resume browsing."""
         self.checkbox_label = tk.Label(self, text="If checked, the following URLs will pause the browser:")
         self.checkbox_label.grid(row=0, column=6, padx=self.x_pad, pady=self.y_pad, sticky="W")
 
@@ -97,24 +113,17 @@ class GlastoGUI(tk.Tk):
         self.scrollbar.grid(row=0, column=7, rowspan=20, sticky="ns")
 
         self.checkbox_canvas.configure(yscrollcommand=self.scrollbar.set)
-
-        # Create frame inside canvas to place checkbuttons
         self.frame = tk.Frame(self.checkbox_canvas)
         self.checkbox_canvas.create_window((0, 0), window=self.frame, anchor="nw")
+        self.checkbox_canvas.bind('<Configure>', self._on_canvas_configure)
 
-        # Update the scroll region on configuration changes
-        def on_configure(event):
-            self.checkbox_canvas.configure(scrollregion=self.checkbox_canvas.bbox('all'))
-
-        self.checkbox_canvas.bind('<Configure>', on_configure)
-        self.checkboxes = []
-        self.checkvars = []
-
-        self.geometry("")
-        self.mainloop()
+    def _on_canvas_configure(self, event):
+        """Updates the scroll region on configuration changes."""
+        self.checkbox_canvas.configure(scrollregion=self.checkbox_canvas.bbox('all'))
 
     def update_checkboxes(self, data):
-        # Get latest value from existing checkboxes and update the data:
+        """Updates checkboxes based on input data."""
+        # Get latest value from existing checkboxes and update the data
         for i, checkbox in enumerate(self.checkboxes):
             data[checkbox.cget("text")] = bool(self.checkvars[i].get())
         self.manager.desired_page = data
@@ -122,57 +131,56 @@ class GlastoGUI(tk.Tk):
         # Clear the frame first
         for widget in self.frame.winfo_children():
             widget.destroy()
-        self.checkboxes = []
-        self.checkvars = []
+        self.checkboxes.clear()
+        self.checkvars.clear()
 
-        # Now, create and update checkboxes based on input data
+        # Create and update checkboxes
         for i, (checkbox_name, checked_value) in enumerate(data.items()):
             var = tk.IntVar(value=1 if checked_value else 0)
             cb = tk.Checkbutton(self.frame, text=checkbox_name, variable=var)
-            cb.grid(row=i, column=0, sticky="w")  # Assuming self.x_pad and self.y_pad values can be used here
+            cb.grid(row=i, column=0, sticky="w")
             self.checkboxes.append(cb)
             self.checkvars.append(var)
 
     def start_managers(self, driver_count, url):
+        """Starts the manager with specified driver count and URL."""
         self.manager = GlastoManager(interface=self, driver_count=driver_count)
         self.manager.form_grid()
         self.manager.start(url)
         self.manager.quit()
 
     def monitor_urls(self):
+        """Monitors and updates URLs continuously."""
         while True:
             if self.manager:
-                # Get current URLs:
                 for index, driver in enumerate(self.manager.drivers):
                     try:
                         url = driver.current_url
-                    except Exception:
+                    except AttributeError:
                         url = "None"
 
                     try:
                         self.driver_info[index]["url"].config(text=url)
-                    except Exception as e:
+                    except (KeyError, IndexError) as e:
                         print(f"Error updating url for driver {index}: {e}")
 
-                # Get current list of URLs:
                 self.update_checkboxes(self.manager.desired_page)
 
-            time.sleep(0.5)
+            time.sleep(1)
 
     def start(self):
+        """Starts execution and URL monitoring."""
         driver_count = int(self.driver_count_entry.get())
-        entry_url = self.url_entry.get()
+        url = self.url_entry.get()
 
-        # Start Chromedrivers:
-        self.execution_thread = threading.Thread(target=self.start_managers, args=(driver_count, entry_url))
-        self.execution_thread.start()
-
+        # Initialize driver_info list to avoid any previous data
         self.driver_info = []
+
         for driver in range(driver_count):
             label = tk.Label(self, text=f"Driver {driver + 1} - searching")
             label.grid(row=driver + 3, column=0, padx=self.x_pad, pady=self.y_pad, sticky="W")
-            url = tk.Label(self, text="")
-            url.grid(row=driver + 3, column=1, columnspan=3, padx=self.x_pad, pady=self.y_pad, sticky="W")
+            url_label = tk.Label(self, text="")
+            url_label.grid(row=driver + 3, column=1, columnspan=3, padx=self.x_pad, pady=self.y_pad, sticky="W")
             resume_button = tk.Button(self, text=f"Resume", command=lambda d=driver: self.manager.resume_searching(d))
             resume_button.grid(row=driver + 3, column=4, padx=self.x_pad, pady=self.y_pad, sticky="W")
             open_button = tk.Button(self, text=f"Open", command=lambda d=driver: self.manager.set_focus(d))
@@ -182,26 +190,20 @@ class GlastoGUI(tk.Tk):
                 "label": label,
                 "resume_button": resume_button,
                 "open_button": open_button,
-                "url": url
+                "url": url_label  # Renamed this from 'url' to 'url_label' for clarity
             })
 
+        # Dynamic window geometry setup, if required
         self.geometry("")
 
+        # Starting the execution and URL monitoring threads
+        self.execution_thread = threading.Thread(target=self.start_managers, args=(driver_count, url))
+        self.execution_thread.start()
+
         self.url_thread = threading.Thread(target=self.monitor_urls)
+        self.url_thread.daemon = True
         self.url_thread.start()
 
-    def destroy(self):
-        if self.manager:
-            self.manager.quit()
-
-        if self.execution_thread:
-            while self.execution_thread.is_alive():
-                try:
-                    self.execution_thread.join(timeout=1)
-                except Exception as e:
-                    print(f"Error in thread {self.execution_thread.name}: {e}")
-
-        super(GlastoGUI, self).destroy()
 
 
 class GlastoManager:
